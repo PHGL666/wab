e5bd066f231d79b1ca482883ef8cd8cac3123067
Gotzsche <pierregotzsche@gmail.com>
2019-12-02 15:51:20 +0100

    Admin install

--- src/Controller/BuilderController.php
+++ src/Controller/BuilderController.php
@@ -7,32 +7,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BuilderController extends AbstractController
 {
-    /** 
-     * @Route("/", name="home")
-     */
-    public function home() 
-    {
-        return $this->render('builder/home.html.twig', [
-            'controller_name' => 'BuilderController',
-        ]);
-    }
-
-    /**
-     * @Route("/wab-project", name="wab-project")
-     */
-    public function wab_project()
-    {
-        return $this->render('builder/wab-project.html.twig', [
-            'controller_name' => 'BuilderController',
-        ]);
-    }
-
     /**
      * @Route("/builder", name="builder")
      */
     public function index()
     {
-        return $this->render('builder/index.html.twig', [
+        return $this->render('builder/builder.html.twig', [
             'controller_name' => 'BuilderController',
         ]);
     }
