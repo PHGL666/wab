ef39636f3c45d7c543527b50b1e3bfa9019a2945
Gotzsche <pierregotzsche@gmail.com>
2019-12-06 11:43:41 +0100

    création propriété slug dans article

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -41,6 +41,11 @@ class Article
      */
     private $createdAt;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -90,6 +95,18 @@ class Article
     public function setCreatedAt(\DateTimeInterface $createdAt): self
     {
         $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
 
         return $this;
     }
